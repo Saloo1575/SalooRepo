745c7be3fc08f26f0d6f4948d96214eb0d2b9edb
Saloo1575 <154738646+Saloo1575@users.noreply.github.com>
2026-08-25 04:11:34 +0300

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -3,30 +3,16 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
 
-// Use an integer for version numbers
 version = 1
 
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
+    description = "SalooRepo test provider"
+    authors = listOf("Saloo1575")
 
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
+    status = 1
     tvTypes = listOf("Movie")
-
     requiresResources = true
     language = "en"
-
-    // Random CC logo I found
     iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
 }
 
