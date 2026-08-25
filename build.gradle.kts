f36f170412f185ff11a831d3e5e4f7557d0d104b
Saloo1575 <154738646+Saloo1575@users.noreply.github.com>
2026-08-25 03:27:09 +0300

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("com.android.library") version "8.5.2" apply false
+    kotlin("android") version "2.0.21" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
